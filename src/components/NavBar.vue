3359d14d587429c9e10a5e477c3b0299ce6b0ccf
YuriNaoki <dev.peterson.machado@gmail.com>
2024-08-24 17:06:30 -0300

    Responsividade 1

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,12 +3,12 @@
     <router-link class="fw-bolder" style="color: white; text-decoration: none;" to="/">Only Study</router-link>
     <div>
       <nav class="d-flex align-items-center">
-        <router-link to="/login" class="login-link d-flex align-items-center me-4 text-decoration-none fw-bold">
-          <img class="me-1" src="../components/icons/Customer.png" alt="">
+        <router-link to="/login" class="btn-nav login-link d-flex align-items-center me-4 text-decoration-none fw-bold">
+          <img class="me-1" src="@/components/icons/Customer.png" alt="">
           LOG IN
         </router-link>
         <router-link to="/register"
-          class="register-link px-4 py-2 fw-bolder text-white border border-purple rounded text-decoration-none"
+          class="btn-nav register-link px-4 py-2 fw-bolder text-white border border-purple rounded text-decoration-none"
           @click="register"
         >
           CADASTRAR-SE
@@ -45,6 +45,12 @@ nav {
 .register-link:hover {
   background-color: #7D1479;
   border-color: #fff;
+}
+
+@media screen and (max-width: 768px) {
+  .btn-nav {
+    font-size: 0.5rem;
+  }
 }
 
 </style>
